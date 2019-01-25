49290b665acb212372e82994b77889c8e0cab7ce
chenchenxinran <elise.shen@mail.utoronto.ca>
2019-01-25 17:30:21 -0500

    try

--- Assignment1/fishtank/src/fishtank/FishFrame.java
+++ Assignment1/fishtank/src/fishtank/FishFrame.java
@@ -7,8 +7,8 @@ import javax.swing.JFrame;
  */
 public class FishFrame extends JFrame {
 
-  /** My Serializable id.  */
-  private static final long serialVersionUID = 1409191926708912242L;
+    /** My Serializable id.  */
+    private static final long serialVersionUID = 1409191926708912242L;
 
     /**
      * Paints this fish tank.
@@ -20,26 +20,26 @@ public class FishFrame extends JFrame {
         // Get my width and height.
         int w = getBounds().width;
         int h = getBounds().height;
-        
+
         // Paint the window white.
         g.setColor(Color.white);
         g.fillRect(0, 0, w, h);
- 
-			// Tell all the fishtank items to draw themselves.
-            for (int a = 0; a != (int) (480 / 10); a++) {
+
+        // Tell all the fishtank items to draw themselves.
+        for (int a = 0; a != (int) (480 / 10); a++) {
             for (int b = 0; b != (int) (640 / 6); b++) {
-              if (FishTank.myLittleFishies[a][b] != null)
-                  if (FishTank.myLittleFishies[a][b] instanceof Fish) {
-                      ((Fish) FishTank.myLittleFishies[a][b]).draw(g);
-                  }else if (FishTank.myLittleFishies[a][b] instanceof Seaweed) {
-                    ((Seaweed) FishTank.myLittleFishies[a][b]).draw(g);
-                  }else if (FishTank.myLittleFishies[a][b] instanceof HungryFish) {
-                    ((HungryFish) FishTank.myLittleFishies[a][b]).draw(g);
-                  }if (FishTank.myLittleFishies[a][b] instanceof Bubble) {
-                      ((Bubble) FishTank.myLittleFishies[a][b]).draw(g);
-                  }
+                if (FishTank.myLittleFishies[a][b] != null)
+                    if (FishTank.myLittleFishies[a][b] instanceof Fish) {
+                        ((Fish) FishTank.myLittleFishies[a][b]).draw(g);
+                    }else if (FishTank.myLittleFishies[a][b] instanceof Seaweed) {
+                        ((Seaweed) FishTank.myLittleFishies[a][b]).draw(g);
+                    }else if (FishTank.myLittleFishies[a][b] instanceof HungryFish) {
+                        ((HungryFish) FishTank.myLittleFishies[a][b]).draw(g);
+                    }if (FishTank.myLittleFishies[a][b] instanceof Bubble) {
+                    ((Bubble) FishTank.myLittleFishies[a][b]).draw(g);
+                }
             }
-            }
+        }
 
     }
 }
